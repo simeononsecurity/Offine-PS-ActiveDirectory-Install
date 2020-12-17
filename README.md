5504a5ab3aec1f21c610f8fba54ff082d43918dc
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-16 21:08:29 -0600

    Init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Offine-PS-ActiveDirectory-Install
  Install the PowerShell RSAT ActiveDirectory Module Offline
+
+### How to install:
+1. [Download the script](https://github.com/simeononsecurity/Offine-PS-ActiveDirectory-Install/archive/master.zip)
+2. From the extracted folder, run ```.\sos-offlinepsadinstall```
---
+++ sos-offlinepsadinstall.ps1
@@ -0,0 +1,23 @@
+#SimeonOnSecurity - https://simeononsecurity.ch
+#http://woshub.com/using-powershell-active-directory-module-without-installing-rsat/
+
+#Continue on error
+#$ErrorActionPreference = 'silentlycontinue'
+#Require elivation for script run
+#Requires -RunAsAdministrator
+
+#Copy Files
+#Install PowerShell Modules
+Copy-Item -Path .\Files\ActiveDirectory -Destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\" -Force -Recurse
+#Install Required DLLs
+Copy-Item -Path .\Files\WinSxS\* -Destination "C:\Windows\WinSxS\" -Force -Recurse
+
+#Unblock New PowerShell Modules and DLLs
+Get-ChildItem "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ActiveDirectory\" -recurse | Unblock-File
+Unblock-File -Path "C:\Windows\WinSxS\Microsoft.ActiveDirectory.Management.resources.dll"
+Unblock-File -Path "C:\Windows\WinSxS\Microsoft.ActiveDirectory.Management.dll"
+
+#Install PowerShell Modules and DLLs
+Import-Module -Name ActiveDirectory -Force -Global
+Import-Module "C:\Windows\WinSxS\Microsoft.ActiveDirectory.Management.resources.dll" -Force -Global
+Import-Module "C:\Windows\WinSxS\Microsoft.ActiveDirectory.Management.dll" -Force -Global
